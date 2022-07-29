185d100eeda600a055b6799b2f547d014d27bd93
Olajide olanrewaju <olajide.m.olanrewaju@gmail.com>
2022-07-29 11:10:10 +0100

    fixed the year issue added  js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// let copyRight = () => {
+  window.addEventListener("load", function () {
+  let date = new Date();
+  let element = document.getElementById("year");
+  element.innerText = "© " + date.getFullYear();
+  })
+    // "&copy;" +
+    // date.getFullYear();
+// };
+
+//this program was created by ......
